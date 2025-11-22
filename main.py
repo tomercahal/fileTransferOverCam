166f8849ec91409cf5d430ae26a4acb36ed74035
Tomer Cahal <tomercahal@gmail.com>
2025-11-22 03:29:32 +0200

    Moved files for testing to a seperate directory

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from reciver import receiver_main
 from sender import sender_main
 
 def main():
-    print('Hello, Botera!')
+    print('Hello, file-Transfer-over-cam!')
     # receiver_main()
     sender_main()
     # cam = get_web_cam()
